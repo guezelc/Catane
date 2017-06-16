641a6e3a33b6b09e7a1792fab7ff7060ac1c67d4
Unknown <clement.guezel@iut-valence.fr>
2017-06-16 10:47:13 +0200

    FIX addTops

--- Class/mapMatrice.class.js
+++ Class/mapMatrice.class.js
@@ -1,4 +1,4 @@
-/*
+hexaNE !== "null"/*
  * A Catane game map uses matrice structure
  * 
  * Author :
@@ -273,7 +273,7 @@ function MapMatrice() {
     {
         var mat = matrice;
         var tiltX = [-1, -1, 0];
-        var tiltY = [-1, 0, -1];
+        var tiltY = [0, -1, -1];
         var T_hexa = [];
 
         T_hexa.push(getHexagon(modelInfo, matrice, x, y));
@@ -299,19 +299,19 @@ function MapMatrice() {
     {
         var top1 = new Top(hexagon, hexaNE, hexaNW);
         hexagon.T_Top["N"] = top1;
-        if (hexaNE !== null)
+        if (hexaNE !== null && hexaNE !== "null")
         {
             hexaNE.T_Top["S-W"] = top1;
         }
         if (hexaNW !== null)
         {
-            //hexaNW.T_Top["S-E"] = top1;
+            hexaNW.T_Top["S-E"] = top1;
         }
         var top2 = new Top(hexagon, hexaNW, hexaW);
         hexagon.T_Top["N-W"] = top2;
         if (hexaNW !== null)
         {
-            //hexaNW.T_Top["S"] = top2;
+            hexaNW.T_Top["S"] = top2;
         }
         if (hexaW !== null)
         {
@@ -351,13 +351,11 @@ function MapMatrice() {
      */
     function getHexagon(modelInfo, matrice, x, y)
     {
-        var hexagon = null;
-
         if (x >= 0 && y >= 0 && x < modelInfo.line && y <= modelInfo.column)
         {
-            hexagon = matrice[x][y];
+            return matrice[x][y];
         }
-        return hexagon;
+        return null;
     }
 
     /*
